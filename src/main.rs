ff28d78d510cd5c1446673e73b4c9bf3b98b8ac7
staceytay <staceytay@users.noreply.github.com>
2023-10-11 15:20:25 -0500

    Use re-exported std::net::TcpStream in a get example

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,32 @@
 use std::{
-    fs,
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
+    io::{Read, Write},
+    str,
 };
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+use tcp::TcpStream;
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+fn main() -> std::io::Result<()> {
+    let mut stream = TcpStream::connect("example.com:80")?;
 
-        handle_connection(stream);
+    println!("* Connected to {}", stream.peer_addr().unwrap());
+
+    let mut response = [0; 2048];
+
+    let _ = stream.write(b"GET / HTTP/1.1\nHost: example.com\nAccept: */*\n\n");
+    let _ = stream.read(&mut response);
+
+    if let [headers, content] = &(str::from_utf8(&response)
+        .unwrap()
+        .split("\r\n\r\n")
+        .take(2)
+        .collect::<Vec<&str>>())[..]
+    {
+        for h in headers.split("\r\n") {
+            println!("< {}", h);
+        }
+        println!("<");
+        println!("{} ", content);
     }
-}
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
-    };
-
-    let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
-
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-    stream.write_all(response.as_bytes()).unwrap();
+    Ok(())
 }
