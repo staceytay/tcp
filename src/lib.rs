f5419d93491adcb43d812138e48dfb36577527e2
staceytay <staceytay@users.noreply.github.com>
2023-10-19 23:05:41 -0500

    Working implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
+use libc::{
+    c_short, c_uchar, close, ioctl, open, read, write, IFF_NO_PI, IFF_TUN, IFNAMSIZ, O_RDWR,
+};
 use std::{
     io,
     net::{SocketAddr, ToSocketAddrs},
+    os::unix::io::RawFd,
 };
 
 pub struct TcpStream {
@@ -10,6 +14,19 @@ pub struct TcpStream {
 impl TcpStream {
     pub fn connect<T: ToSocketAddrs>(addr: T) -> io::Result<TcpStream> {
         let socket_addr = addr.to_socket_addrs().unwrap().collect::<Vec<_>>()[0];
+
+        let tun = TunSocket::new("tun0")?;
+        println!("TUN: {:?}", tun);
+
+        let syn = b"E\x00\x00,\x00\x01\x00\x00@\x06\xf6\xc7\xc0\x00\x02\x02\xc0\x00\x02\x0109\x1f\x90\x00\x00\x00\x00\x00\x00\x00\x00`\x02\xff\xff\xc4Y\x00\x00\x02\x04\x05\xb4";
+        let res = tun.write(syn)?;
+
+        let mut buf = [0; 1024];
+
+        let packet = tun.read(&mut buf);
+        println!("packet: {:?}", packet.unwrap());
+        println!("buf: {:?}", buf);
+
         Ok(TcpStream { socket_addr })
     }
 
@@ -31,5 +48,69 @@ impl io::Write for TcpStream {
 
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         Ok(42)
+    }
+}
+
+#[derive(Debug)]
+struct TunSocket {
+    fd: RawFd,
+}
+
+impl Drop for TunSocket {
+    fn drop(&mut self) {
+        unsafe { close(self.fd) };
+    }
+}
+
+#[repr(C)]
+union IfrIfru {
+    ifru_flags: c_short,
+}
+
+#[repr(C)]
+pub struct ifreq {
+    ifr_name: [c_uchar; IFNAMSIZ],
+    ifr_ifru: IfrIfru,
+}
+
+impl TunSocket {
+    const TUNSETIFF: u64 = 0x400454CA;
+
+    fn new(name: &str) -> Result<TunSocket, io::Error> {
+        let fd = match unsafe { open(b"/dev/net/tun\0".as_ptr() as _, O_RDWR) } {
+            -1 => return Err(io::Error::last_os_error()),
+            fd => fd,
+        };
+
+        let iface_name = "tun0".as_bytes();
+        let mut ifr = ifreq {
+            ifr_name: [0; IFNAMSIZ],
+            ifr_ifru: IfrIfru {
+                ifru_flags: (IFF_TUN | IFF_NO_PI) as _,
+            },
+        };
+
+        ifr.ifr_name[..iface_name.len()].copy_from_slice(iface_name);
+
+        if unsafe { ioctl(fd, Self::TUNSETIFF as _, &ifr) } < 0 {
+            println!("ioctl error");
+            return Err(io::Error::last_os_error());
+        }
+
+        Ok(TunSocket { fd })
+    }
+
+    fn read<'a>(&self, dst: &'a mut [u8]) -> Result<&'a mut [u8], io::Error> {
+        match unsafe { read(self.fd, dst.as_mut_ptr() as *mut _, dst.len()) } {
+            -1 => Err(io::Error::last_os_error()),
+            n => Ok(&mut dst[..n as usize]),
+        }
+    }
+
+    pub fn write(&self, buf: &[u8]) -> Result<usize, io::Error> {
+        match unsafe { write(self.fd, buf.as_ptr() as *const _, buf.len()) } {
+            -1 => Err(io::Error::last_os_error()),
+            n => Ok(n as usize),
+        }
     }
 }
